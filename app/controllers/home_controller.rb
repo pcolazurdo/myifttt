0e011d319a86b16d39e1bbb1af3d7d6d4c9f3936
unknown <pcolazurdo@nbk065538.telefe.com.ar>
2013-04-12 16:57:45 -0300

    Added Partial Show of Favtweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
+
 class HomeController < ApplicationController
+  def index
+    tf = ApplicationHelper::TweeterFunctions.new()
+    tf.favorites()
+    
+    @favtweets = Favtweet.all
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @favtweets }
+    end
+  end
 end
