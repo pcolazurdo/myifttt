444c72af2f7bdb2f9634cc072ca1290b143d22f6
pcolazurdo <pcolazurdo@yahoo.com>
2013-04-25 22:41:54 -0300

    Fixed some errors. Still work to do.

--- app/helpers/evernotes_helper.rb
+++ app/helpers/evernotes_helper.rb
@@ -26,9 +26,10 @@ module EvernotesHelper
   end
   
   class EvernoteNote
-    
+     
     def initialize ()
-      @authToken = E::ERCFile.active_consumer_key
+      ercfile = E::ERCFile.instance
+      @authToken = ercfile.active_consumer_key
       evernoteHost = "sandbox.evernote.com"
       userStoreUrl = "https://#{evernoteHost}/edam/user"
 
@@ -42,31 +43,67 @@ module EvernotesHelper
       puts "Is my Evernote API version up to date?  #{versionOK}"
       puts
       exit(1) unless versionOK
-      
-    end
-    
-    def save(twit)
       noteStoreUrl = @userStore.getNoteStoreUrl(@authToken)
 
       noteStoreTransport = Thrift::HTTPClientTransport.new(noteStoreUrl)
       noteStoreProtocol = Thrift::BinaryProtocol.new(noteStoreTransport)
-      noteStore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
+      @noteStore = Evernote::EDAM::NoteStore::NoteStore::Client.new(noteStoreProtocol)
 
       # List all of the notebooks in the user's account
-      notebooks = noteStore.listNotebooks(@authToken)
+      notebooks = @noteStore.listNotebooks(@authToken)
       puts "Found #{notebooks.size} notebooks:"
       defaultNotebook = notebooks.first
       notebooks.each do |notebook|
         puts "  * #{notebook.name}"
       end
-
       puts
-      puts "Creating a new note in the default notebook: #{defaultNotebook.name}"
+      puts "Notes will be created in the default notebook: #{defaultNotebook.name}"
       puts
-
+    end
+    
+    def translate_error(e)
+      error_name = "unknown"
+      case e.errorCode
+      when Evernote::EDAM::Error::EDAMErrorCode::AUTH_EXPIRED
+        error_name = "AUTH_EXPIRED"
+      when Evernote::EDAM::Error::EDAMErrorCode::BAD_DATA_FORMAT
+        error_name = "BAD_DATA_FORMAT"
+      when Evernote::EDAM::Error::EDAMErrorCode::DATA_CONFLICT
+        error_name = "DATA_CONFLICT"
+      when Evernote::EDAM::Error::EDAMErrorCode::DATA_REQUIRED
+        error_name = "DATA_REQUIRED"
+      when Evernote::EDAM::Error::EDAMErrorCode::ENML_VALIDATION
+        error_name = "ENML_VALIDATION"
+      when Evernote::EDAM::Error::EDAMErrorCode::INTERNAL_ERROR
+        error_name = "INTERNAL_ERROR"
+      when Evernote::EDAM::Error::EDAMErrorCode::INVALID_AUTH
+        error_name = "INVALID_AUTH"
+      when Evernote::EDAM::Error::EDAMErrorCode::LIMIT_REACHED
+        error_name = "LIMIT_REACHED"
+      when Evernote::EDAM::Error::EDAMErrorCode::PERMISSION_DENIED
+        error_name = "PERMISSION_DENIED"
+      when Evernote::EDAM::Error::EDAMErrorCode::QUOTA_REACHED
+        error_name = "QUOTA_REACHED"
+      when Evernote::EDAM::Error::EDAMErrorCode::SHARD_UNAVAILABLE
+        error_name = "SHARD_UNAVAILABLE"
+      when Evernote::EDAM::Error::EDAMErrorCode::UNKNOWN
+        error_name = "UNKNOWN"
+      when Evernote::EDAM::Error::EDAMErrorCode::VALID_VALUES
+        error_name = "VALID_VALUES"
+      when Evernote::EDAM::Error::EDAMErrorCode::VALUE_MAP
+        error_name = "VALUE_MAP"
+      end
+      rv = "Error code was: #{error_name}[#{e.errorCode}] and parameter: [#{e.parameter}]"
+    end
+    
+    def save(twit)
       # To create a new note, simply create a new Note object and fill in
       # attributes such as the note's title.
       note = Evernote::EDAM::Type::Note.new
+      
+      puts "Saving Note from #{twit.screen_name}"
+      puts "note: #{twit.full_text}"
+      
       note.title = "Tweet from #{twit.screen_name}"
       note.content = <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -78,7 +115,14 @@ EOF
 # Finally, send the new note to Evernote using the createNote method
 # The new Note object that is returned will contain server-generated
 # attributes such as the new note's unique GUID.
-      createdNote = noteStore.createNote(@authToken, note)
+      begin
+        createdNote = @noteStore.createNote(@authToken, note)
+      rescue Evernote::EDAM::Error::EDAMUserException => e
+       #the exceptions that come back from Evernote are hard to read, but really important to keep track of
+        msg = "Caught an exception from Evernote trying to create a note.  #{translate_error(e)}"
+        raise msg
+      end
+      
 
       puts "Successfully created a new note with GUID: #{createdNote.guid}"
     end
